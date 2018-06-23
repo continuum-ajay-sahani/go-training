f93669e72546f8a3e41ac558d2a50c5528e7c4bb
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-23 15:17:17 +0530

    order scope

--- a/scope/order/main.go
+++ a/scope/order/main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 )
 
+func a() {
+	fmt.Println("Inside a")
+}
+
 func main() {
 
 	//fmt.Println(x)
 	// this will give error
 	x := 5
 	fmt.Println(x)
+	a()
+	test()
+}
+
+func test() {
+	fmt.Println("Inside Test")
 }
