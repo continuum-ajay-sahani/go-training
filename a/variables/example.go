9cb6252db181af883c3e4e62c14fbab8213aa8d5
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-23 18:44:09 +0530

    added type in example

--- a/variables/example.go
+++ a/variables/example.go
@@ -9,10 +9,15 @@ var b, c int = 10, 5
 var d, e = "Morning", true
 var f float64
 
+type myData float64
+
 func main() {
 	f = 1.234
 	e := "Afternoon"
 	var g = "test"
+
+	var per myData
+	per = 8.96
 
 	fmt.Println("a-", a)
 	fmt.Println("b-", b)
@@ -22,10 +27,12 @@ func main() {
 	fmt.Println("f-", f)
 	fmt.Println("g-", g)
 	fmt.Println(increment(5))
+	fmt.Println(per)
 
 	// formtat specifier
 	fmt.Printf("data type of a=%T\n", a)
 	fmt.Printf("value of a = %v\n", a)
+	fmt.Printf("per type = %T\n", per)
 }
 
 var increment = func(a int) int {
