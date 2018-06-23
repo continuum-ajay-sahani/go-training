fe70f588b84f5a5fb2eb325ff743fb08c655b4b7
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-23 21:24:27 +0530

    added switch cases

--- b/switch/main.go
+++ b/switch/main.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	//switchStatementNoMatch()
+	//switchStatementMatch()
+	//fallThrough()
+	//multipleValue()
+	noExpression()
+}
+
+func switchStatementNoMatch() {
+	switch "Ajay" {
+	case "Raj":
+		fmt.Println("Raj Here")
+	case "Vijay":
+		fmt.Println("Vijay")
+	default:
+		fmt.Println("Nothing Match")
+
+	}
+}
+
+func switchStatementMatch() {
+	switch "Ajay" {
+	case "Ajay":
+		fmt.Println("Ajay Here")
+	case "Vijay":
+		fmt.Println("Vijay")
+	default:
+		fmt.Println("Nothing Match")
+
+	}
+}
+
+func fallThrough() {
+	switch "Ajay" {
+	case "Ajay":
+		fmt.Println("Ajay Here")
+		fallthrough
+	case "Ravi":
+		fmt.Println("Ravi Here")
+	case "Vijay":
+		fmt.Println("Vijay")
+	default:
+		fmt.Println("Nothing Match")
+
+	}
+}
+
+func multipleValue() {
+	switch "Sanjay" {
+	case "Ajay", "Sanjay", "Ovais":
+		fmt.Println("Ajay,Sanjay,Ovais Here")
+	case "Ravi", "Cricket":
+		fmt.Println("Ravi Here")
+	case "Vijay":
+		fmt.Println("Vijay")
+	default:
+		fmt.Println("Nothing Match")
+
+	}
+}
+
+func noExpression() {
+	name := "Ajay"
+	switch {
+	case len(name) < 3:
+		fmt.Println("Ravi Here")
+	case name == "Ajay":
+		fmt.Println("Ajay Here")
+	case name == "Ravi":
+		fmt.Println("Ravi Here")
+	case name == "Vijay":
+		fmt.Println("Vijay")
+	default:
+		fmt.Println("Nothing Match")
+
+	}
 }
