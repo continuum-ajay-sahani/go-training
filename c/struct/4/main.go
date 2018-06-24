f47937a4fbcc065ac0d6299a3e91956139456676
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-24 11:10:25 +0530

    struct with pointer

--- c/struct/4/main.go
+++ c/struct/4/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	p := &person{"Ajay", "Sahani", 29}
+	fmt.Println(p.GetName())
+	p.UpdateFirst()
+	fmt.Println(p.GetName())
+	p.UpdateLast()
+	fmt.Println(p.GetName())
+}
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func (p person) GetName() string {
+	return fmt.Sprintf("%s %s", p.First, p.Last)
+}
+
+func (p person) UpdateFirst() {
+	p.First = "Raj"
+}
+
+func (p *person) UpdateLast() {
+	p.Last = "Kumar"
 }
