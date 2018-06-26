87ae74ad07bed07219ae119258fcb22c834be548
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-26 15:19:10 +0530

    added anon method

---
+++ d/anon/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+}
