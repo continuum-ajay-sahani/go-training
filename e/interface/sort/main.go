91fa8b161dea2ca70ff0dbb6a717d080b0962262
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-06-27 10:00:50 +0530

    Added interface

---
+++ e/interface/sort/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type people []string
+
+func (p people) Len() int           { return len(p) }
+func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p people) Less(i, j int) bool { return p[i] < p[j] }
+
+func main() {
+	studyGroup := people{"Ajay", "Raj", "Sam", "Ram"}
+
+	fmt.Println(studyGroup)
+	sort.Sort(studyGroup)
+	fmt.Println(studyGroup)
+
+}
